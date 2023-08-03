62383babe17d3b9459fa2c66f2f53cd94332b048
Piyaporn <piyapornlahr@gmail.com>
2023-08-03 14:47:14 +0100

    Trying on firebase hosting

---
+++ firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBOFsMcGN8SAReivaigInFTXzKSqgP9iP0",
+  authDomain: "my-journey-backend.firebaseapp.com",
+  projectId: "my-journey-backend",
+  storageBucket: "my-journey-backend.appspot.com",
+  messagingSenderId: "596535939916",
+  appId: "1:596535939916:web:a3af9ec7c8c2472efbf5e9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
